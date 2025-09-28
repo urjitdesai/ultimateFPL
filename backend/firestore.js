3db6a8201f8960a1063ec05236717bfcb391435e
urjitdesai <urjitdesai1998@gmail.com>
2025-09-27 19:48:25 -0700

    remove comment

--- backend/firestore.js
+++ backend/firestore.js
@@ -1,13 +1,11 @@
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
-// import JSON service account using assert (requires Node >= 18)
 import serviceAccount from './firebase-service-account-key.json' with { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// Use DATABASE_ID from env, or FIRESTORE_DATABASE_ID, or default to 'ultimatefpl'
 const databaseId = process.env.FIREBASE_DATABASE_ID ;
 const db = getFirestore(admin.app(), databaseId);
 console.log('Database id in firestore= ', databaseId);
